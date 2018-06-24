eb03b7273b89dbf391f076a2c1fdd32413d3c87f
Andres Manco <andresmanco@gmail.com>
2018-06-24 06:32:08 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  dict =   {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2', 
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
+    "and" => "&"
+  }
+end
+
+
+
+def word_substituter(tweet_one)
+  dictionary.keys.collect do |d|
+    tweet_one.gsub! d  do |s|
+      if s.length == d.length
+        dictionary[d]
+      end
+    end
+  end 
+  tweet_one
+end
